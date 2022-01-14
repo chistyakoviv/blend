54022e8f017d0b3ad1c9a9580536c1d320e1cc9e
Ilya Chistyakov <ichistyakovv@gmail.com>
2022-01-14 21:06:47 +0500

    Added the watch mode

--- setup/gulpfile.js
+++ setup/gulpfile.js
@@ -2,8 +2,13 @@ const gulp = require('gulp');
 const config = require('../src/config/config');
 const blend = require('../src/blend');
 const lazyRequireTask = require('../lazyRequireTask');
+const yargs = require('yargs');
+const { hideBin } = require('yargs/helpers');
+
+const argv = yargs(hideBin(process.argv)).argv
 
 config.isDev = !process.env.NODE_ENV || process.env.NODE_ENV == 'dev';
+config.isWatching = Boolean(argv.watch);
 
 require(blend.path());
 
@@ -21,7 +26,7 @@ lazyRequireTask('watch', './src/tasks/watch');
 
 const taskList = ['clean', gulp.parallel('assets', 'sass', 'webpack'), 'html'];
 
-if (config.isDev) {
+if (config.isDev && config.isWatching) {
     taskList.push(gulp.parallel('server', 'watch'));
 }
 
